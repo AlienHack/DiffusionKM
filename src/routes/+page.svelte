64e41bd055859850355ede5fda04df41ebdada22
XenoCoreZ <the4got10@windowslive.com>
2023-03-21 22:58:32 +0700

    Update caption

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -61,6 +61,15 @@
 	/>
 </svelte:head>
 
+<div class="flex flex-col justify-center items-center mb-4">
+	<h1 class="prose text-4xl hover:text-cyan-600 transition-all duration-300">
+		Stable Diffusion Thailand Knowledge Portal
+	</h1>
+	<h3 class="prose text-xl mt-2 hover:text-cyan-600 transition-all duration-300">
+		แหล่งรวบรวมข้อมูลความรู้เกี่ยวกับ Stable Diffusion
+	</h3>
+</div>
+
 <div class="grid grid-cols-4 gap-2">
 	<input
 		type="text"
@@ -80,6 +89,7 @@
 		{/each}
 	</select>
 </div>
+
 <div class="overflow-x-auto w-full">
 	<table class="table w-full">
 		<!-- head -->
