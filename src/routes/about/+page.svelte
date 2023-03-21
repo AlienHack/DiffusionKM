ae50554c6c2395227bc0915102ef9ec5ccc63f97
XenoCoreZ <the4got10@windowslive.com>
2023-03-22 00:06:37 +0700

    Update about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -35,6 +35,23 @@
 		<div class="card-body">
 			<h2 class="card-title">เกี่ยวกับ</h2>
 			<p>เว็บไซต์นี้สร้างขึ้นเพื่อเป็นแหล่งข้อมูลการค้นหา และจัดทำ Stable Diffusion</p>
+			<p class="prose prose-xl font-bold">Projects</p>
+			<ul class="prose">
+				<li>
+					<a href="https://sdkm.code.productions">Stable Diffusion KM</a>
+				</li>
+				<li>
+					<a href="https://content.code.productions" target="_blank">Blogs</a>
+				</li>
+				<li>
+					<a href="https://fiction.code.productions" target="_blank">Fictionlog Downloader</a>
+				</li>
+				<li>
+					<a href="https://www.facebook.com/code.productions.ready" target="_blank"
+						>Code.Productions</a
+					>
+				</li>
+			</ul>
 		</div>
 	</div>
 </div>
